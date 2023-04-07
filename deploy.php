bd385ad7b5ae1f4425c600e51c00387153300726
Jitin Joseph <jitin.joseph@toptechinfo.com>
2023-04-07 12:51:59 +0530

    test

---
+++ deploy.php
@@ -0,0 +1,22 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Config
+
+set('repository', 'https://github.com/jitin-joseph/php-deploy-test.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('185.151.48.42')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/deploy_test');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
